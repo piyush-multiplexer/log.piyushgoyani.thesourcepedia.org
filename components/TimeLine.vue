d0a9c65029e4c769688cd0c4e2cee37b5dc5e9ec
Piyush Goyani <piyush.goyani@ezeetechnosys.com>
2022-04-29 14:04:48 +0530

    init: add vuetimeline lib and remove unsed

---
+++ components/TimeLine.vue
@@ -0,0 +1,16 @@
+<template>
+  <vue-timeline-update
+    :animation="true"
+    animation-container=""
+    :animation-duration="1500"
+    category=""
+    color="blue"
+    :date="new Date('2020-01-02')"
+    description="Hello"
+    icon=""
+    :is-last="false"
+    thumbnail=""
+    theme="light"
+    title="With"
+  ></vue-timeline-update>
+</template>
