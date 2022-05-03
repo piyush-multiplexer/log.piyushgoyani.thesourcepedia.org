919e65e7187814af6f8a77f30438616ad8bf1195
Piyush Goyani <piyush.goyani@ezeetechnosys.com>
2022-05-03 11:02:14 +0530

    db: reconfig  - primary key autoincrement  -

--- api/db.js
+++ api/db.js
@@ -3,11 +3,10 @@ const db = new sqlite3.Database('pg-logs.db')
 module.exports = {
     db,
     createTable: function createTable() {
-        db.run('CREATE TABLE log (id INTEGER,createdAt CHARACTER(20),date CHARACTER(20),title VARCHAR(255),description TEXT)')
+        db.run('CREATE TABLE log (id INTEGER PRIMARY KEY AUTOINCREMENT, createdAt CHARACTER(20), date CHARACTER(20), title VARCHAR(255), description TEXT)')
     },
-    addLog: function addLog(id, timestamp, title, description) {
-        const q = `INSERT INTO log (id, timestamp, title, description) VALUES(${id}, ${timestamp}, "${title}", "${description}")`
-        console.log(q)
+    addLog: function addLog(createdAt, date, title, description) {
+        const q = `INSERT INTO log (createdAt, date, title, description) VALUES(${createdAt}, "${date}", "${title}", "${description}")`
         db.run(q)
     },
     close: function close() {
