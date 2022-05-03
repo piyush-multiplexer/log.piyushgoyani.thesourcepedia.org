c9a6daad89519a2e0e094c344d72b6015f33da18
Piyush Goyani <piyush.goyani@ezeetechnosys.com>
2022-05-03 14:13:27 +0530

    ui: alert added

--- components/AddLog.vue
+++ components/AddLog.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="m-2 p-6">
+    <div
+      v-if="isAlert"
+      class="flex p-4 mb-4 bg-green-100 border-t-4 border-green-500 dark:bg-green-200"
+    >
+      <div class="ml-3 text-sm font-medium text-green-700">
+        Activity Logged!
+      </div>
+      <button
+        class="ml-auto -mx-1.5 -my-1.5 bg-green-100 dark:bg-green-200 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 dark:hover:bg-green-300 inline-flex h-8 w-8"
+        aria-label="Close"
+        @click="isAlert = false"
+      >
+        <svg
+          class="w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </button>
+    </div>
     <div class="mb-6">
       <VueTailWindPicker
         :theme="theme"
@@ -89,6 +115,7 @@ export default {
       title: '',
       description: '',
       url: 'http://localhost:3000/api/',
+      isAlert: false,
       theme: {
         background: '#FFFFFF',
         text: 'text-gray-700',
@@ -127,6 +154,7 @@ export default {
           date: new Date(this.date).toDateString(),
         }),
       })
+      this.isAlert = true
       this.title = ''
       this.description = ''
       this.date = ''
