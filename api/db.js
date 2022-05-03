5d0cebcd7c46b0aa9f88f8bc1b51a60ca8ab86c8
Piyush Goyani <piyush.goyani@ezeetechnosys.com>
2022-05-03 10:45:05 +0530

    db: restructure db  - add date field and rename

--- api/db.js
+++ api/db.js
@@ -1,9 +1,9 @@
 const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database('test.db')
+const db = new sqlite3.Database('pg-logs.db')
 module.exports = {
     db,
     createTable: function createTable() {
-        db.run('CREATE TABLE log (id INTEGER,timestamp CHARACTER(20),title VARCHAR(255),description TEXT)')
+        db.run('CREATE TABLE log (id INTEGER,createdAt CHARACTER(20),date CHARACTER(20),title VARCHAR(255),description TEXT)')
     },
     addLog: function addLog(id, timestamp, title, description) {
         const q = `INSERT INTO log (id, timestamp, title, description) VALUES(${id}, ${timestamp}, "${title}", "${description}")`
