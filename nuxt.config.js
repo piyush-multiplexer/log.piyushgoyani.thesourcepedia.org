af95e324f0b44d933f5abd123f6043bc4949b8f4
Piyush Goyani <piyush.goyani@ezeetechnosys.com>
2022-05-04 10:06:28 +0530

    update site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'log.piyushgoyani.thesourcepedia.org',
+    title: 'Activity Log - Piyush Goyani',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
